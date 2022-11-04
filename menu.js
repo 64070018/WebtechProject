cc261b6faee2d4e7ba50b77e6cfca12c2fa58229
Chanakarn <88476014+64070018@users.noreply.github.com>
2022-11-04 13:47:15 +0700

    Add files via upload

--- menu.js
+++ menu.js
@@ -4,30 +4,48 @@ function addToCart(value) {
 
     products.forEach((product) => {
         if (product.id == value) {
-            showMenu.innerHTML += `
-                <div class="container">
-                    <div class="picmenu">
-                        <!-- left container -->
-                        <img src="${product.img}" width="70%">
-                        <br>
-                        <label for="name">${product.menu}</label>
-                        <br><br>
-                        <label for="detail">${product.description}</label>
-                    </div>
-                    <div class="optionmain">
-                        <!-- right container -->
-                        แป้งเครป
-                        <img src="https://media.discordapp.net/attachments/892267785244254229/1033705703786958868/bleach.png" onclick="swax(1)" width="20px" align="right" id="drop1" class="dropdown">
-                        <hr>
-                        <div id="flour">
-                            <input type="checkbox" class="form-check-input">วนิลา<br>
-                            <input type="checkbox" class="form-check-input">ชาโคล<br>
-                            <input type="checkbox" class="form-check-input">ทูโทน<br>
-                        </div>`
+            // showMenu.innerHTML += `
+            //     <div class="menu-container">
+            //         <div class="picmenu">
+            //             <!-- left container -->
+            //             <img src="${product.img}" width="70%">
+            //             <br>
+            //             <label for="name">${product.menu}</label>
+            //             <br><br>
+            //             <label for="detail">${product.description}</label>
+            //         </div>
+            //         <div class="optionmain">
+            //             <!-- right container -->
+            //             แป้งเครป
+            //             <img src="https://media.discordapp.net/attachments/892267785244254229/1033705703786958868/bleach.png" onclick="swax(1)" width="20px" align="right" id="drop1" class="dropdown">
+            //             <hr>
+            //             <div id="flour">
+            //                 <input type="checkbox" class="form-check-input">วนิลา<br>
+            //                 <input type="checkbox" class="form-check-input">ชาโคล<br>
+            //                 <input type="checkbox" class="form-check-input">ทูโทน<br>
+            //             </div>`
 
             if (value <= 15) {
                 //crap
-                showMenu.innerHTML += `
+                showMenu.innerHTML += `<div class="menu-container">
+                <div class="picmenu">
+                    <!-- left container -->
+                    <img src="${product.img}" width="70%">
+                    <br>
+                    <label for="name">${product.menu}</label>
+                    <br><br>
+                    <label for="detail">${product.description}</label>
+                </div>
+                <div class="optionmain">
+                    <!-- right container -->
+                    แป้งเครป
+                    <img src="https://media.discordapp.net/attachments/892267785244254229/1033705703786958868/bleach.png" onclick="swax(1)" width="20px" align="right" id="drop1" class="dropdown">
+                    <hr>
+                    <div id="flour">
+                        <input type="checkbox" class="form-check-input">วนิลา<br>
+                        <input type="checkbox" class="form-check-input">ชาโคล<br>
+                        <input type="checkbox" class="form-check-input">ทูโทน<br>
+                    </div>
                         <br>
                         ท้อปปิ้ง
                         <img src="https://media.discordapp.net/attachments/892267785244254229/1033705703786958868/bleach.png" width="20px" align="right" onclick="swax(2)" class="dropdown" id="drop2">
@@ -47,33 +65,38 @@ function addToCart(value) {
                         </div>
                     </div>
                 </div>
-
-                        }
             `;
             } else if (value <= 20) {
                 // combo set
-                showMenu.innerHTML += `
+                showMenu.innerHTML += `<div class="menu-container">
+                <div class="picmenu">
+                    <!-- left container -->
+                    <img src="${product.img}" width="70%">
+                    <br>
+                    <label for="name">${product.menu}</label>
+                    <br><br>
+                    <label for="detail">${product.description}</label>
+                </div>
+                <div class="optionmain">
+                    <!-- right container -->
+                    ความหวาน
+                    <img src="https://media.discordapp.net/attachments/892267785244254229/1033705703786958868/bleach.png"
+                        onclick="swax(1)" width="20px" align="right" id="drop1" class="dropdown">
+                    <hr>
+                    <div id="flour">
+                        <input type="checkbox" class="form-check-input">หวานมาก<br>
+                        <input type="checkbox" class="form-check-input">หวานปกติ<br>
+                        <input type="checkbox" class="form-check-input">หวานน้อย<br>
+                    </div>
+                    <div id="count">
                         <br>
-                        ท้อปปิ้ง
-                        <img src="https://media.discordapp.net/attachments/892267785244254229/1033705703786958868/bleach.png" width="20px" align="right" onclick="swax(2)" class="dropdown" id="drop2">
-                        <hr>
-                        <div id="topping">
-                            <input type="checkbox" class="form-check-input">combooo<br>
-                            <input type="checkbox" class="form-check-input">combooo<br>
-                            <input type="checkbox" class="form-check-input">combooo<br>
-                            <input type="checkbox" class="form-check-input">combooo<br>
-                        </div>
-                        <div id="count">
-                            <br>
-                            จำนวน
-                        </div>
-                        <div id="add">
-                            <button class="bt" onclick="add()">เพิ่ม</button>
-                        </div>
+                        จำนวน
+                    </div>
+                    <div id="add">
+                        <button class="bt" onclick="add()">เพิ่ม</button>
                     </div>
                 </div>
-
-                        }
+            </div>
             `;
 
             } else if (value <= 30) {
@@ -99,7 +122,6 @@ function addToCart(value) {
             </div>
         </div>
 
-                }
     `;
 
             } else {
@@ -124,8 +146,6 @@ function addToCart(value) {
                 </div>
             </div>
         </div>
-
-                }
     `;
 
             }
